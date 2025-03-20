528fea2b31304518f2fd9c1c01a84dfcc0a8a17a
PolyakovAn <PolyakovAn@terralink-global.com>
2025-03-20 17:59:53 +0500

    Спринт 14. Реализация апи для работы с пользователями и предметами с хранением в оперативной памяти. Проходит тесты постман, нужно написать юнит-тесты

--- src/main/java/ru/practicum/shareit/item/dto/Item.java
+++ src/main/java/ru/practicum/shareit/item/dto/Item.java
@@ -1,18 +1,17 @@
 package ru.practicum.shareit.item.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class ItemDto {
+public class Item {
     private Long id;
-    @NotBlank
     private String name;
-    @NotBlank
+
     private String description;
-    @NotNull
     private Boolean available;
+    private Long owner;
+
+    private Long request;
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,36 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
 
-/**
- * TODO Sprint add-controllers.
- */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public UserDto findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto create(@RequestBody @Valid UserDto user) {
+        return userService.create(user);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto update(@PathVariable Long id,
+                          @RequestBody UserDto user) {
+        return userService.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        userService.delete(id);
+    }
 }
